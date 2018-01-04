7b095ac5ae5f74eb473ed7a868681886d043f5be
yuhei.k <yuhei.k@gmail.com>
2018-01-04 17:27:20 +0900

    Implement a Custom Control : Add support for Interface Builder.
    Add @IBDesignable and others. Seems some are not working.

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -7,11 +7,23 @@
 
 import UIKit
 
-class RatingControl: UIStackView {
+@IBDesignable class RatingControl: UIStackView {
 
     //MARK: Properties
     private var ratingButtons = [UIButton]()
     var rating = 0
+
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+        didSet {
+            setupButtons()
+        }
+    }
+
+    @IBInspectable var starCount: Int = 5 {
+        didSet {
+            setupButtons()
+        }
+    }
 
     //MARK: Initialization
     override init(frame: CGRect) {
@@ -33,15 +45,23 @@ class RatingControl: UIStackView {
 
     //MARK: Private Methods
     private func setupButtons() {
-        for _ in 0..<5 {
+        // clear any existing buttons
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+
+
+        for _ in 0..<starCount {
             // Create the button
             let button = UIButton()
             button.backgroundColor = UIColor.red
 
             // Add Constraints
             button.translatesAutoresizingMaskIntoConstraints = false
-            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
 
             // Setup the button action
             button.addTarget(self,
