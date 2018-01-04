7e8b94c4bb44c653dd82d1bf3d866859e1bab138
yuhei.k <yuhei.k@gmail.com>
2018-01-04 14:52:05 +0900

    Implement a Custom Control : Create a Custom View.
    Add new file for custom view.

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,28 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Yuhei Kikuchi on 2018/01/04.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
