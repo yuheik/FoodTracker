254eb00349933aea1c180e2c4def00675c319e32
yuhei.k <yuhei.k@gmail.com>
2018-01-04 17:01:18 +0900

    Implement a Custom Control : Add Buttons to the View
    Add rating buttons and its actions.

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -8,13 +8,52 @@
 import UIKit
 
 class RatingControl: UIStackView {
+
+    //MARK: Properties
+    private var ratingButtons = [UIButton]()
+    var rating = 0
+
     //MARK: Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupButtons()
     }
 
     required init(coder: NSCoder) {
         super.init(coder: coder)
+        setupButtons()
+    }
+
+
+    //MARK: Button Action
+    func ratingButtonTapped(button: UIButton) {
+        print("Button Pressed 👍")
+    }
+
+
+    //MARK: Private Methods
+    private func setupButtons() {
+        for _ in 0..<5 {
+            // Create the button
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
+
+            // Add Constraints
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+
+            // Setup the button action
+            button.addTarget(self,
+                             action: #selector(RatingControl.ratingButtonTapped(button:)),
+                             for: .touchUpInside)
+
+            // Add the button to the stack
+            addArrangedSubview(button)
+
+            // Add the new button to the rating button array
+            ratingButtons.append(button)
+        }
     }
 
     /*
