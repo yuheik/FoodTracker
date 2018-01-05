6f4bf58d3ee14766ffbcee453cdfeca569f64450
yuhei.k <yuhei.k@gmail.com>
2018-01-06 01:28:22 +0900

    Implement a Custom Control : Add Accessibility information.
    For blind users.

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -75,7 +75,7 @@ import UIKit
         let emptyStar       = UIImage(named: "emptyStar",       in: bundle, compatibleWith: self.traitCollection)
         let highlightedStar = UIImage(named: "highlightedStar", in: bundle, compatibleWith: self.traitCollection)
 
-        for _ in 0..<starCount {
+        for index in 0..<starCount {
             // Create the button
             let button = UIButton()
 
@@ -89,6 +89,9 @@ import UIKit
             button.translatesAutoresizingMaskIntoConstraints = false
             button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
             button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
+
+            // Set the accessiblity label
+            button.accessibilityLabel = "Set \(index + 1) star rating"
 
             // Setup the button action
             button.addTarget(self,
@@ -109,6 +112,29 @@ import UIKit
         for (index, button) in ratingButtons.enumerated() {
             // If the index of a button is less than the rating, the button should be selected.
             button.isSelected = (index < rating)
+
+            // Set the hint string for the currently selected star
+            let hintString: String?
+            if rating == index + 1 {
+                hintString = "Tap to reset the rating to zero"
+            } else {
+                hintString = nil
+            }
+
+            // Calculate the value string
+            let valueString: String
+            switch (rating) {
+            case 0:
+                valueString = "No rating set"
+            case 1:
+                valueString = "1 star set"
+            default:
+                valueString = "\(rating) stars set"
+            }
+
+            // Assign the hint string and value string
+            button.accessibilityHint = hintString
+            button.accessibilityValue = valueString
         }
     }
 
