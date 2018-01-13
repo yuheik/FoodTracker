4ee33bde676efc35c2469fa65c50439978b0037c
yuhei.k <yuhei.k@gmail.com>
2018-01-13 13:22:11 +0900

    Create a Table View : Design Custom Table Cells
    Customised design.

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Yuhei Kikuchi on 2018/01/13.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
