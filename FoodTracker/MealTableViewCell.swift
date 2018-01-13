139046a16b626578cfba4064a5c31937cd5aeb19
yuhei.k <yuhei.k@gmail.com>
2018-01-13 14:27:27 +0900

    Create a Table View : Connect the Table Cell UI to Code
    Add reference connection.

--- FoodTracker/MealTableViewCell.swift
+++ FoodTracker/MealTableViewCell.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class MealTableViewCell: UITableViewCell {
+    // MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
 
     override func awakeFromNib() {
         super.awakeFromNib()
