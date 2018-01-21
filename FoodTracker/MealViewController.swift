9794d079b938fbbaaa70d6c2ce2ca3e61fd1c256
yuhei.k <yuhei.k@gmail.com>
2018-01-21 13:14:26 +0900

    Implement Navigation : Cancel a New Meal Addition.
    Implement Cancel button action.

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -94,6 +94,9 @@ class MealViewController: UIViewController,
     }
 
     //MARK: Navigation
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
 
     // This method lets you configure a view controller before it's presented.
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
