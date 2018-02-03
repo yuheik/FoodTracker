e7bbb3ca0c9755ecf77d82eaa485d3d307e63df2
yuhei.k <yuhei.k@gmail.com>
2018-02-03 12:42:20 +0900

    Customize description value of Meal.

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -15,6 +15,12 @@ class Meal: NSObject, NSCoding {
     var photo: UIImage?
     var rating: Int
 
+    override var description: String {
+        get {
+            return "\"\(name)\""
+        }
+    }
+
     // MARK: Archiving Paths
 
     static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
